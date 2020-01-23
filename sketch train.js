0f14cc31431e62367d9cd959656f37cbd4700ab4
Taha Bouhsine <skywolf.mo@gmail.com>
2020-01-23 12:19:13 +0100

    all the new model and implementation
    all files

---
+++ sketch train.js
@@ -0,0 +1,165 @@
+let video;
+let poseNet;
+let poses = [];
+
+let skeleton;
+let brain;
+let state = 'waiting';
+let targetLabel;
+
+let bgMusic;
+
+function preload() {
+    bgMusic = loadSound('bgMusic.mp3');
+}
+
+
+
+function getPoses(resposes) {
+    poses = resposes;
+    if (poses.length > 0) {
+        if (state == 'collecting') {
+            pose = poses[0].pose;
+            // // console.log(pose.keypoints[0]);
+            // let inputs = [];
+            // for (let i = 0; i < pose.keypoints.length; i++) {
+            //     let keypoint = pose.keypoints[i];
+            //     // console.log(keypoint);
+
+            //     // let x = pose.keypoints[i].position.x;
+            //     // let y = pose.keypoints[i].position.y;
+            //     inputs.push(keypoint.position.x);
+            //     inputs.push(keypoint.position.y);
+
+            // }
+            // let target = [targetLabel];
+            // brain.addData(inputs, target);
+        }
+    }
+}
+
+
+// function keyPressed() {
+
+//     if (key == 's') {
+//         brain.saveData();
+//     }
+//     else {
+//         targetLabel = key;
+//         console.log(key);
+//         p1 = new Promise((res, rej) => {
+//             setTimeout(res, 5000);
+//         })
+//             .then(() => {
+//                 setState();
+//                 console.log(state);
+
+//                 p2 = new Promise((res, rej) => {
+//                     setTimeout(res, 5000);
+//                 })
+//                     .then(() => {
+//                         setState();
+//                         console.log(state);
+
+//                     });
+//             });
+
+//         // setTimeout(() => {
+//         //     setState();
+//         //     console.log(state);
+
+//         //     setTimeout(() => {
+//         //         setState();
+//         //         console.log(state);
+//         //     }, 5000);
+
+//         // }, 5000);
+
+
+
+
+//     }
+// }
+function setState() {
+    if (state == 'collecting') {
+        state = 'waiting';
+    }
+    else {
+        state = 'collecting';
+    }
+}
+
+function setup() {
+    // bgMusic.setVolume(0.5);
+    // bgMusic.loop();
+
+    createCanvas(640, 480);
+    video = createCapture(VIDEO);
+    video.size(width, height);
+    video.hide();
+    // poseNet = ml5.poseNet(video, modelReady);
+    // poseNet.on('pose', getPoses);
+    let options = {
+        inputs: 14,
+        outputs: 4,
+        task: 'classification',
+        debug: true
+    }
+    brain = ml5.neuralNetwork(options);
+    brain.loadData('iniha_data.json', dataReady);
+}
+
+function dataReady() {
+    brain.normalizeData();
+    brain.train({epochs: 500}, finished);
+}
+function finished(){
+    console.log('model trained');
+    brain.save();
+    
+}
+function modelReady() {
+    select('#status').html('Model Loaded');
+}
+
+function draw() {
+    image(video, 0, 0, width, height);
+
+    // We can call both functions to draw all keypoints and the skeletons
+    drawKeypoints();
+    drawSkeleton();
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+    // Loop through all the poses detected
+    for (let i = 0; i < poses.length; i++) {
+        // For each pose detected, loop through all the keypoints
+        let pose = poses[i].pose;
+        for (let j = 0; j < pose.keypoints.length; j++) {
+            // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+            let keypoint = pose.keypoints[j];
+            // Only draw an ellipse is the pose probability is bigger than 0.2
+            if (keypoint.score > 0.2) {
+                fill(255, 0, 0);
+                noStroke();
+                ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+            }
+        }
+    }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+    // Loop through all the skeletons detected
+    for (let i = 0; i < poses.length; i++) {
+        let skeleton = poses[i].skeleton;
+        // For every skeleton, loop through all body connections
+        for (let j = 0; j < skeleton.length; j++) {
+            let partA = skeleton[j][0];
+            let partB = skeleton[j][1];
+            stroke(255, 0, 0);
+            line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+        }
+    }
+}
